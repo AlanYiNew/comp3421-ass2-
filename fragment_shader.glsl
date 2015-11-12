f4d424c148aca1377886b57724b4ffa18989f2ec
Alan YI <245655664@qq.com>
2015-11-12 22:40:44 +1100

    Update fragment_shader.glsl

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,3 +1,4 @@
+//shader with texture
 #version 130
 
 in vec3 normal;
@@ -39,4 +40,68 @@ void main (void) {
 	specular = clamp(specular,0,1);
     gl_FragColor = texture2D(color,texture) * (gl_FrontMaterial.emission + globalAmbient + ambient + diffuse) + specular;
 	
+}
+
+
+
+
+
+
+
+//shader without texture
+#version 130
+
+in vec3 normal;
+in vec4 world;
+ 
+uniform int light;
+
+void main (void) {	
+    vec4 ambient, globalAmbient;
+    
+	ambient =  gl_LightSource[light].ambient * gl_FrontMaterial.ambient;
+	globalAmbient = gl_LightModel.ambient * gl_FrontMaterial.ambient;
+
+	vec3 n, lightDir; 
+	vec4 diffuse;
+	float NdotL;
+	
+	n = normalize(normal);
+	
+	lightDir = normalize(vec3(gl_LightSource[light].position - world));
+    NdotL = max(dot(n, lightDir), 0.0); 
+    diffuse = NdotL * gl_FrontMaterial.diffuse * gl_LightSource[light].diffuse; 
+
+    vec4 specular = vec4(0.0,0.0,0.0,1);
+    float NdotHV;
+    float NdotR;
+    vec3 dirToView = normalize(vec3(-world));
+    
+    vec3 R = normalize(reflect(-lightDir,n)); 
+    vec3 H =  normalize(lightDir+dirToView); 
+    
+	if (NdotL > 0.0) {
+		NdotR = max(dot(R,dirToView ),0.0);
+		specular = gl_FrontMaterial.specular * gl_LightSource[light].specular * pow(NdotHV,gl_FrontMaterial.shininess);
+	    
+	}
+	
+	specular = clamp(specular,0,1);
+    gl_FragColor = gl_FrontMaterial.emission + globalAmbient + ambient + diffuse + specular;
+	
+}
+
+
+//vertext shader
+#version 130
+
+out vec3 normal; 
+out vec4 world; 
+out vec2 texture;
+
+void main (void) {	
+    world = gl_ModelViewMatrix * gl_Vertex;
+    normal = vec3(normalize(gl_NormalMatrix * normalize(gl_Normal)));
+	texture = vec2(gl_MultiTexCoord0);
+	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;	
 }
